f610053f009c9308998ac45e748ad7e5df4f2bd3
Nitin <getnpk@gmail.com>
2017-07-23 18:09:45 +0800

    Add mdToast and fake contact

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,15 @@
 
     angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+
+        // dummy data till we have a user
+        var contact = {
+            name: 'Nitin',
+            phone: '+65 88888888',
+            email: 'fake@gmail.com'
+        };
+
         classifiedsFactory.getClassifieds()
             .then(function(classifieds) {
                 $scope.classifieds = classifieds.data;
@@ -19,7 +27,16 @@
 
         $scope.saveClassified = function(classified) {
             if (classified) {
+                classified.contact = contact;
                 $scope.classifieds.push(classified);
+                $scope.classified = {};
+                $scope.closeSidebar();
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content("Saved classified")
+                        .position('top, right')
+                        .hideDelay(2000)
+                );
             }
         }
     });
