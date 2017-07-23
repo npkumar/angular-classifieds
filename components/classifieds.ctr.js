54e76e4807426e27b95f2ed836e4f11b03f0bac5
Nitin <getnpk@gmail.com>
2017-07-23 18:41:05 +0800

    Edit and editSave

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -31,13 +31,30 @@
                 $scope.classifieds.push(classified);
                 $scope.classified = {};
                 $scope.closeSidebar();
-                $mdToast.show(
-                    $mdToast.simple()
-                        .content("Saved classified")
-                        .position('top, right')
-                        .hideDelay(2000)
-                );
+                showToast('Classified Saved!');
             }
+        }
+
+        $scope.editClassified = function(classified) {
+            $scope.editing = true;
+            $scope.openSidebar();
+            $scope.classified = classified;
+        }
+
+        $scope.saveEdit = function() {
+            $scope.editing = false;
+            $scope.classified = {};
+            $scope.closeSidebar();
+            showToast('Classified Edit Saved!');
+        }
+
+        function showToast(message) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .content(message)
+                    .position('top, right')
+                    .hideDelay(2000)
+            );
         }
     });
 })();
